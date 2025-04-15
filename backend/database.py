862932613c7e8efb027720f0fef9b068a1d79c29
tpbn <pabillarantrishamay2024@gmail.com>
2025-04-15 09:09:57 +0800

    addededited

--- backend/database.py
+++ backend/database.py
@@ -3,7 +3,11 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = os.environ["DATABASE_URL"]
+SQLALCHEMY_DATABASE_URL = os.environ.get(
+    "DATABASE_URL",
+    "postgresql://postgres:spidermanBatmanWolverine@127.0.0.1:5432/todo_db"
+)
+
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
